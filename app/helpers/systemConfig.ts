845a0e487398588369c10261824c64e7bfdf083a
Dipsy Wong <ycwongal@connect.ust.hk>
2023-08-21 15:09:26 +0800

    update default values

--- app/helpers/systemConfig.ts
+++ app/helpers/systemConfig.ts
@@ -33,8 +33,8 @@ function env(key: string, fallback: EnvType): EnvType {
 const config = {
   APP_NAME: env('APP_NAME', name),
   APP_URL: env('APP_URL', `https://${env('VERCEL_URL', 'localhost:3000')}`),
-  MINESWEEPER_UI_URL: env('MINESWEEPER_UI_URL', '/cis-minesweeper-ui/index.html'),
-  MINESWEEPER_UI_DOWNLOAD_URL: env('MINESWEEPER_UI_DOWNLOAD_URL', '/cis-minesweeper-ui.zip'),
+  MINESWEEPER_UI_URL: env('MINESWEEPER_UI_URL', 'https://cis2023-minesweeper-4c07151a1697.herokuapp.com/cis-minesweeper-ui/index.html'),
+  MINESWEEPER_UI_DOWNLOAD_URL: env('MINESWEEPER_UI_DOWNLOAD_URL', 'https://cis2023-minesweeper-4c07151a1697.herokuapp.com/cis-minesweeper-ui.zip'),
   COORDINATOR_TOKEN: env('COORDINATOR_TOKEN', ''),
   API_KEY: env('API_KEY', 'api-key'),
   ENABLE_FAKE_STUDENT: env('ENABLE_FAKE_STUDENT', false),
