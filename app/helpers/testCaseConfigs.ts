8162b060a9b76c54d19ff0fcc19f5af61aa6ab82
Dipsy Wong <ycwongal@connect.ust.hk>
2023-08-22 15:25:39 +0800

    update number of mines

--- app/helpers/testCaseConfigs.ts
+++ app/helpers/testCaseConfigs.ts
@@ -16,10 +16,16 @@ export const getTestCaseConfigs  = (): ITestCaseConfig[] => {
     score: 5
   })),
   
-  Array(8).fill('').map(() => ({
-    columns: randomInt(16, 61),
-    rows: randomInt(16, 33),
-    mines: 10,
-    score: 10
-  })))
+  Array(8).fill('').map(() => {
+    const columns = randomInt(16, 61)
+    const rows = randomInt(16, 33)
+    const nCells = columns * rows
+    const mines = Math.floor((nCells - 256) * (99-40)/(480-256) + 40)
+    return {
+      columns,
+      rows,
+      mines,
+      score: 10
+    }
+  }))
 }
