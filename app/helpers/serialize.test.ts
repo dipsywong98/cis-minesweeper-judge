b28117feb3ff43083984e5a721c31f6b9049b083
Dipsy Wong <ycwongal@connect.ust.hk>
2023-09-07 21:31:40 +0800

    fix static sample

---
+++ app/helpers/serialize.test.ts
@@ -0,0 +1,21 @@
+import { deserialize, serialize } from "./serialize"
+
+describe('serialize', () => {
+  it('sample1', () => {
+    expect(serialize(2, 2, '1001')).toEqual('MiwyLHdwQT0=')
+  })
+
+  it('sample2', () => {
+    expect(serialize(3, 4, '101001000000')).toEqual('Myw0LHdxUUE=')
+  })
+})
+
+describe('deserialize', () => {
+  it('sample1', () => {
+    expect(deserialize('MiwyLHdwQT0=')).toEqual('1001')
+  })
+
+  it('sample2', () => {
+    expect(deserialize('Myw0LHdxUUE=')).toEqual('101001000000')
+  })
+})
